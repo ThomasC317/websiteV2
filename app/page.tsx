70dc1b01b1a7a1179906ba2a3f0c85cb350f4e98
Thomas CERDERA <thomas.cerdera@campus-igs-toulouse.fr>
2024-07-06 11:36:44 +0200

    updating gitignore + command prompt component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
@@ -5,6 +6,10 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import Navbar from "@/components/component/navbar"
 import CommandPrompt from "@/components/component/commandPrompt"
+const handleButtonClick = () => {
+  window.location.href = '/work';
+};
+
 export default function Home() {
   return (
     <div className="min-h-screen flex gap-main-page items-center justify-center py-12 bg-gray-100">
@@ -20,8 +25,11 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <Button variant="outline" className="w-full sm:w-auto">
-              Mes expériences
+            <Button variant="outline" className="w-full sm:w-auto" onClick={handleButtonClick}>
+              Mon travail
+            </Button>
+            <Button variant="outline" className="w-full sm:w-auto" style={{ backgroundColor: 'black', color: 'white' }}>
+              Télécharger mon CV
             </Button>
           </div>
         </main>
@@ -29,30 +37,6 @@ export default function Home() {
       <div className="w-full max-w-2xl bg-white bg-opacity-80 p-8 md:p-12 lg:p-16 rounded-lg shadow-lg mt-12">
         <div className="mx-auto space-y-8">
         <CommandPrompt />
-          {/* <div className="space-y-4">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Get in Touch</h2>
-            <p className="text-lg text-gray-700 md:text-xl">
-              I'd love to hear from you. Whether you have a project in mind or just want to say hello, feel free to
-              reach out.
-            </p>
-          </div>
-          <form className="space-y-4">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="John Doe" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="john@example.com" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="message">Message</Label>
-              <Textarea id="message" rows={4} placeholder="Your message..." />
-            </div>
-            <Button type="submit" className="w-full">
-              Send Message
-            </Button>
-          </form> */}
         </div>
       </div>
     </div>);
--- components/component/commandPrompt.tsx
+++ components/component/commandPrompt.tsx
@@ -1,56 +1,91 @@
-"use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CommandPrompt = () => {
   const [input, setInput] = useState('');
-  const [output, setOutput] = useState([
-    { command: '', result: '> Bienvenue sur mon site web ! \n > Voici la liste des commandes : \n > - hello \n > - date \n > - presentation \n > - technologies \n > - animals \n > - travels \n > - contact' }
-  ]);
+  const [output, setOutput] = useState([]);
+
+
+  const getFormattedDate = () => {
+    const months = [
+      "janvier", "février", "mars", "avril", "mai", "juin",
+      "juillet", "août", "septembre", "octobre", "novembre", "décembre"
+    ];
   
+    const date = new Date();
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+    const formattedDate = `Nous sommes le ${day} ${month} ${year} et il est actuellement ${hours}:${minutes}:${seconds}`;
+  
+    return formattedDate;
+  };
+  const [currentTime, setCurrentTime] = useState(getFormattedDate());
+  useEffect(() => {
+    // Initialize output with the list of commands on component mount
+    processCommand('command');
+    
+    // Set interval to update current time every second
+    const interval = setInterval(() => {
+      setCurrentTime(getFormattedDate());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval); // Cleanup interval on component unmount
+    };
+  }, []);
+
   const handleInputChange = (event) => {
     setInput(event.target.value);
   };
 
   const handleInputSubmit = (event) => {
     event.preventDefault();
-    processCommand(input);
+    processCommand(input.trim());
     setInput('');
   };
 
   const processCommand = (command) => {
+    let newOutput = [];
     let result;
+
     switch (command) {
       case 'hello':
-        result = 'ThomasCerdera> Hello, World!';
+        result = '> Hello, World!';
         break;
-      case 'date':
-        result = new Date().toString();
+      case 'presentation':
+        result = `> Je m'appelle Thomas, j'ai 27 ans et je suis Ingénieur développeur. \n > J'aime la photographie, la musique, le cinéma, les voyages...`;
         break;
-    case 'presentation':
-        result = `> Je m'appelle Thomas, j'ai 27 ans et je suis Ingénieur développeur. \n > J'aime la photographie, la musique, le cinéma, les voyages...`
+      case 'technologies':
+        result = `> J'ai récemment découvert le React, avec du Next.js et j'ai beaucoup apprécié, d'où la création de ce site. En dehors de ça, je maîtrise (plus ou moins) l'Angular, les technologies .NET... Vous pouvez trouver plus de détails dans la partie "Mon Travail".`;
         break;
-    case 'technologies':
-        result = `> J'ai récement découvert le React, avec du nextJs et j'ai beaucoup apprécié, d'ou la création de ce site. En dehors de ça je maîtrise (plus ou moins) l'Angular, les technologies .NET... Vous pouvez trouver plus de détails dans la partie "Mon Travail"`;
+      case 'animals':
+        result = `> J'ai actuellement un chat, qui s'appelle Tia ! Elle a deux ans !`;
         break;
-    case 'animals':
-        result = `> J'ai actuellement un chat, qui s'appelle Tia ! elle a deux ans !`;
+      case 'travels':
+        result = `> Le dernier voyage que j'ai fait était l'année dernière, je suis parti en Norvège ! \n > Vous pouvez trouver plus de détails dans la partie "Blog".`;
         break;
-    case 'travels': 
-        result = `> Le dernier voyage que j'ai fait était l'année dernière, je suis parti en norvège ! \n > Vous pouvez trouver plus de détails dans la partie "Blog"`;
+      case 'contact':
+        result = `> Vous pouvez trouver toutes les infos pour me contacter dans la partie "Contact".`;
         break;
-    case 'contact':
-        result = `> Vous pouvez trouver toute les infos pour me contacter dans la partie "Contact`;
+      case 'command':
+        result = `> Voici la liste des commandes : \n > - hello \n > - date \n > - presentation \n > - technologies \n > - animals \n > - travels \n > - contact \n > - clear`;
         break;
       default:
         result = `> Commande non reconnue: ${command}`;
     }
-    setOutput([...output, { command, result }]);
+
+    // Update the output state
+    setOutput([...newOutput, { command, result }]);
   };
 
   const renderOutput = (text) => {
     return text.split('\n').map((item, index) => (
       <React.Fragment key={index}>
- {item}
+        {item}
         <br />
       </React.Fragment>
     ));
@@ -59,6 +94,10 @@ const CommandPrompt = () => {
   return (
     <div style={styles.container}>
       <div style={styles.output}>
+        <div>
+          > Bonjour et bienvenue sur mon site Web ! <br />
+          > {currentTime}
+        </div>
         {output.map((entry, index) => (
           <div key={index}>
             <div>{entry.command ? `> ${entry.command}` : ''}</div>
@@ -79,33 +118,33 @@ const CommandPrompt = () => {
 };
 
 const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      fontFamily: 'monospace',
-      backgroundColor: '#000',
-      color: '#0f0',
-      padding: '10px',
-      borderRadius: '5px',
-      height: '40rem',
-    },
-    output: {
-      flex: 1,
-      overflowY: 'auto',
-      marginBottom: '10px'
-    },
-    form: {
-      display: 'flex'
-    },
-    input: {
-      flex: 1,
-      padding: '10px',
-      backgroundColor: '#000',
-      color: '#0f0',
-      border: '1px solid #0f0',
-      borderRadius: '5px'
-    }
-  };
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    fontFamily: 'monospace',
+    backgroundColor: '#000',
+    color: '#fff',
+    padding: '10px',
+    borderRadius: '5px',
+    height: '40rem',
+  },
+  output: {
+    flex: 1,
+    overflowY: 'auto',
+    marginBottom: '10px'
+  },
+  form: {
+    display: 'flex'
+  },
+  input: {
+    flex: 1,
+    padding: '10px',
+    backgroundColor: '#000',
+    color: '#fff',
+    border: '1px solid #fff',
+    borderRadius: '5px'
+  }
+};
 
 export default CommandPrompt;
