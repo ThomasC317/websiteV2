ac3ceb09bdfe86c24a7182464246b40300c0109e
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-07-08 10:47:12 +0200

    fix export on about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/component/navbar";
+import React, { useState } from 'react';
+const About = () => {
+
+    return (  <div className="min-h-screen bg-white">
+        <Navbar />bonjour</div>);
+  
+  };
+  
+  export default About;
