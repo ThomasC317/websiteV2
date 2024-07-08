a760521a4d5fca643c39f99b1cefdc90ad490b35
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-07-08 16:51:37 +0200

    about text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,16 +11,28 @@ const About = () => {
         <Navbar />
         <div className="w-full max-w-4xl bg-white bg-opacity-80 p-8 md:p-12 lg:p-16 rounded-lg">
         <div className="py-4">
+        <h2 className="text-2xl mb-4">A Propos</h2>
             <div  className="mb-4 flex">
-            
-            <a href="https://www.linkedin.com/in/thomas-cerdera-305ab3153/" className="underline-link"> Linkedin </a>
+            <p className="ml-2">
+                      Je m'appelle Thomas CERDERA, j'ai 27 ans. J'ai commencé ma carrière en tant qu'alternant développeur Windev en License, et ça a été une des pires expériences
+                      de ma vie. J'ai malgré tout perséveré et j'ai trouvé une alternance pour suivre le master Chef de projet ingénierie Logicielle à l'IPI de Blagnac, branche informatique
+                      du groupe IGS. Durant ces deux années, j'ai appris énormément, j'ai pris confiance en moi et j'ai pu travailler sur des projets intéressants. 
+
+                      Je suis maintenant ingénieur Développeur Web, actuellement chez Infotel, ESN travaillant avec de gros clients, et j'ai déjà réalisés quelques projets pour cette entreprise.
+
+                      En dehors de cela, j'ai une bonne maîtrise de l'anglais (920 au TOEIC), j'ai regardé beaucoup de séries en anglais et j'ai été aidé par mes différents voyages.
+
+                      J'apprécie beaucoup le travail en équipe, mais quand il le faut je sais aussi travailler en autonomie. Je suis aussi minutieux et créatif, 
+                      et j'ai acquis des compétences, qui, j'en suis certain, vous seront très utiles.
+                    </p>
+            {/* <a href="https://www.linkedin.com/in/thomas-cerdera-305ab3153/" className="underline-link"> Linkedin </a>
             <Trema/>
                     <p className="ml-2">
                       Vous pouvez retrouver le lien de mon linkedin.
-                    </p>
+                    </p>*/}
       
              
-              </div>
+              </div> 
               <div  className="mb-4 flex">
             
             <a href="https://github.com/ThomasC317" className="underline-link"> Github </a>
