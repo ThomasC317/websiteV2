c044582d82ad43bb2ba4841d953b9e0055ac0499
Thomas CERDERA <thomas.cerdera@campus-igs-toulouse.fr>
2024-07-15 23:24:09 +0200

    update style on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,9 +8,9 @@ import Trema from "@/components/component/trema";
 const About = () => {
 
     return (  
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen flex flex-col bg-white">
         <Navbar isMainPage={false} />
-        <div style={{ position: "relative" }}>
+        <div>
             <ParticleBackground />
             <main className="w-full bg-transparent bg-opacity-80 mx-auto rounded-lg px-24 py-12 mt-24 relative z-10">
                 <div className="mb-4 flex flex-col">
@@ -18,13 +18,15 @@ const About = () => {
                         Thomas CERDERA <Trema/> Ingénieur développeur Web 
                     </h1>
                     <p className="text-lg">
-                        Ingénieur développeur Full-stack situé sur Toulouse, France.<br/>
-                        je travaille actuellement chez Infotel Conseil à Blagnac. J'ai choisi cette agence car elle m'a permis de réaliser des projets dans le domaine de l'aéronautique.<br/>
+                        Ingénieur développeur Full-stack situé sur Toulouse, France.<br/><br/>
+                        je travaille actuellement chez Infotel Conseil à Blagnac. J'ai choisi cette agence car elle m'a permis de réaliser des projets dans le domaine de l'aéronautique.<br/><br/>
                         J'ai une bonne maîtrise de l'anglais (920 au toeic), je suis créatif, curieux, et minutieux.
                     </p>
                 </div>
+<div className="border-t border-color-lightgray py-4">
                 <h2 className="text-xl font-bold mb-2">Vous avez une question, ou une demande ? N'hésitez pas :</h2>
                 <Contact />
+                </div>
             </main>
             <Footer />
         </div>
