cfaca9ee13befcdd8d61a60ee5b32d8badf54c14
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-07-18 14:54:29 +0200

    making home page & navbar responsive

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const handleButtonClick = () => {
   return (
     <div className="min-h-screen flex xl:flex-row flex-col items-center md:justify-center justify-between py-12 bg-gray-100 xl:mt-16">
     <Navbar isOnMainPage={true} />
-    <div className="w-full max-w-4xl bg-white bg-opacity-80 p-8 md:p-12 lg:p-16 rounded-lg shadow-lg mt-24 mx-4">
+    <div className="w-full max-w-4xl bg-white bg-opacity-80 p-8 md:p-12 lg:p-16 rounded-lg shadow-lg mt-24 mx-4 xl:mt-0">
       <main className="flex flex-col gap-8">
         <div className="space-y-4 text-center sm:text-left">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
@@ -43,10 +43,10 @@ const handleButtonClick = () => {
             Je suis un développeur Full-Stack, actuellement très intéressé pour développer mes compétences en Front.
           </p>
         </div>
-        <div className="flex gap-4 sm:items-center">
+        <div className="flex flex-col sm:flex-row gap-4 sm:items-center">
   <Button
     variant="outline"
-    className="w-full sm:w-auto bg-azure-radiance-950 text-white rounded-3xl flex items-center justify-center px-6 py-4 text-2xl border-0"
+    className="w-full sm:w-auto bg-azure-radiance-950 text-white rounded-3xl flex items-center justify-center px-6 py-6 text-2xl border-0"
     onClick={handleButtonClick}
     style={{color:"white", backgroundColor: '#172e54' }}
 >
@@ -66,7 +66,7 @@ const handleButtonClick = () => {
         />
     </svg>
 </Button>
-          <Button variant="outline" className="w-full sm:w-auto bg-azure-radiance-950 text-white rounded-3xl flex items-center justify-center px-6 py-4 text-2xl border border-azure-radiance-950" style={{ color: 'black', backgroundColor: 'white', borderWidth: "1px" }}>
+          <Button variant="outline" className="w-full sm:w-auto bg-azure-radiance-950 text-white rounded-3xl flex items-center justify-center px-6 py-6 text-2xl border border-azure-radiance-950" style={{ color: 'black', backgroundColor: 'white', borderWidth: "1px" }}>
             Télécharger mon CV
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M210.78,39.25l-130.25-23A16,16,0,0,0,62,29.23l-29.75,169a16,16,0,0,0,13,18.53l130.25,23h0a16,16,0,0,0,18.54-13l29.75-169A16,16,0,0,0,210.78,39.25ZM178.26,224h0L48,201,77.75,32,208,55ZM89.34,58.42a8,8,0,0,1,9.27-6.48l83,14.65a8,8,0,0,1-1.39,15.88,8.36,8.36,0,0,1-1.4-.12l-83-14.66A8,8,0,0,1,89.34,58.42ZM83.8,89.94a8,8,0,0,1,9.27-6.49l83,14.66A8,8,0,0,1,174.67,114a7.55,7.55,0,0,1-1.41-.13l-83-14.65A8,8,0,0,1,83.8,89.94Zm-5.55,31.51A8,8,0,0,1,87.52,115L129,122.29a8,8,0,0,1-1.38,15.88,8.27,8.27,0,0,1-1.4-.12l-41.5-7.33A8,8,0,0,1,78.25,121.45Z"></path></svg>
           </Button>
